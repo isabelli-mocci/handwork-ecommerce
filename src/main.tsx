9b6de3069e0448ca7b2b1b3392588a9164f73bf6
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-07-17 20:51:05 -0300

    feat: set up main entry point with routing
    Configured React app entry point with BrowserRouter and defined routes for all main pages, including Home, Products, Product Detail, About, Contact, and NotFound. Ensures proper navigation and structure for the application.

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-import HomePage from './pages/HomePage.tsx';
-import ProductsPage from './pages/ProductsPage.tsx';
-import ProductDetailPage from './pages/ProductDetailPage.tsx';
-import AboutUsPage from './pages/AboutUsPage.tsx';
-import ContactUsPage from './pages/ContactUsPage.tsx';
-import NotFoundPage from './pages/NotFoundPage.tsx';
+import HomePage from './pages/Home/HomePage.tsx';
+import ProductsPage from './pages/Product/ProductsPage.tsx';
+import ProductDetailPage from './pages/ProductDetail/ProductDetailPage.tsx';
+import AboutUsPage from './pages/About/AboutUsPage.tsx';
+import ContactUsPage from './pages/Contact/ContactUsPage.tsx';
+import NotFoundPage from './pages/NotFound/NotFoundPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Envolve todo o aplicativo com o roteador */}
+    <BrowserRouter>
       {' '}
-      <App> {/* pode conter elementos como Header/Footer */}
+      <App> 
         {' '}
-        <Routes> {/* Define as diferentes rotas */}
+        <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
