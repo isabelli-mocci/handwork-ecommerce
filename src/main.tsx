4e6d4be98592c86e225d99e03914950a32372dde
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-07-29 14:59:27 -0300

    feat(router): add FAQ page route and import\n\n- Import FAQPage component\n- Add /faq route to application router

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import ContactUsPage from './pages/ContactUsPage.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage.tsx';
 import ShippingReturnsPage from './pages/ShippingReturnsPage.tsx';
+import FAQPage from './pages/FAQPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -28,6 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="*" element={<NotFoundPage />} />
           <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
           <Route path="/shipping-returns" element={<ShippingReturnsPage />} />
+          <Route path="/faq" element={<FAQPage />} />
         </Routes>
       </App>
     </BrowserRouter>
