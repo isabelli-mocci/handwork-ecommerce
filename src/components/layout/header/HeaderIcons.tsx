937a5c936f399615d84181de0f05304c3aa1a055
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-07-30 12:23:01 -0300

    feat(header): add navigation to cart on shopping bag icon click
    Use useNavigate to redirect to /cart when the shopping bag icon is clicked in HeaderIcons.\nImproves user experience and navigation flow.

--- src/components/layout/header/HeaderIcons.tsx
+++ src/components/layout/header/HeaderIcons.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { HiOutlineHeart, HiOutlineShoppingBag } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
 
-const HeaderIcons: React.FC = () => (
-  <div className='flex items-center gap-1 lg:ml-auto'>
-    <button className='text-xl lg:mr-3'>
-      <HiOutlineHeart aria-label='Wishlist' />
-    </button>
-    <button className='text-text-color text-xl p-2'>
-      <HiOutlineShoppingBag aria-label='Shopping Cart' />
-    </button>
-  </div>
-);
+const HeaderIcons: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <div className='flex items-center gap-1 lg:ml-auto'>
+      <button className='text-xl lg:mr-3'>
+        <HiOutlineHeart aria-label='Wishlist' />
+      </button>
+      <button
+        className='text-text-color text-xl p-2'
+        onClick={() => navigate('/cart')}
+      >
+        <HiOutlineShoppingBag aria-label='Shopping Cart' />
+      </button>
+    </div>
+  );
+};
 
 export default HeaderIcons;
