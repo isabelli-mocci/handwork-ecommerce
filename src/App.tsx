3cef106723c32fe981d105e14f97e1834e0143a8
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-08-03 00:17:57 -0300

    feat(app): add Toaster for global toast notifications and improve layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { type ReactNode } from 'react';
 import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 import { FavoritesProvider } from './hooks/useFavorites';
+import { Toaster } from 'react-hot-toast';
 
 type AppProps = {
   children: ReactNode;
@@ -10,6 +11,12 @@ type AppProps = {
 const App = ({ children }: AppProps) => (
   <FavoritesProvider>
     <div className="min-h-screen flex flex-col">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: { borderRadius: '1rem', fontSize: '1rem' },
+        }}
+      />
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
