d2bf1fa3f498f82fc8384a0361f314886d29602a
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-08-04 18:44:11 -0300

    refactor(wishlist): translate wishlist constants to English
    - Translated all wishlist strings to English
    - Improved consistency and internationalization support

---
+++ src/constants/wishlist.constants.ts
@@ -0,0 +1,6 @@
+export const WISHLIST_STRINGS = {
+  EMPTY_TITLE: 'Your wishlist is empty',
+  EMPTY_DESCRIPTION: 'Explore our collection and add your favorite products here!',
+  EXPLORE_BUTTON: 'Explore Products',
+  PAGE_TITLE: 'My Wishlist'
+} as const;
