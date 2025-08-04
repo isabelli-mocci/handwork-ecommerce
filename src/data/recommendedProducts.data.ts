5a4ce1668b34a823b62e211853bb770df7e46849
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-08-04 20:09:28 -0300

    chore(data): update recommended products mock data
    - Added/updated recommended products in mock data file
    - Ensured consistent structure and formatting for product objects

---
+++ src/data/recommendedProducts.data.ts
@@ -0,0 +1,62 @@
+export const recommendedProductsMock = [
+  {
+    id: '1',
+    name: 'Bear Lady Velvet',
+    image: '/src/assets/images/bear-lady-velvet.png',
+    price: 199
+  },
+  {
+    id: '2',
+    name: 'Bunny Dahlia',
+    image: '/src/assets/images/bunny-dahlia.png',
+    price: 175
+  },
+  {
+    id: '4',
+    name: 'Cat Lord Crimson',
+    image: '/src/assets/images/cat-lord-crimson.png',
+    price: 180
+  },
+  {
+    id: '9',
+    name: 'Dog Gigi',
+    image: '/src/assets/images/dog-gigi.png',
+    price: 170
+  },
+  {
+    id: '11',
+    name: 'Elephant Princess Pearl',
+    image: '/src/assets/images/elephant-princess-pearl.png',
+    price: 210
+  },
+  {
+    id: '12',
+    name: 'Fox Celeste',
+    image: '/src/assets/images/fox-celeste.png',
+    price: 165
+  },
+  {
+    id: '13',
+    name: 'Giraffe Sylvie',
+    image: '/src/assets/images/giraffe-sylvie.png',
+    price: 185
+  },
+  {
+    id: '14',
+    name: 'Monkey Sunny',
+    image: '/src/assets/images/monkey-sunny.png',
+    price: 168
+  },
+  {
+    id: '16',
+    name: 'Owl Penelope',
+    image: '/src/assets/images/owl-penelope.png',
+    price: 150
+  },
+  {
+    id: '17',
+    name: 'Penguin Blossom',
+    image: '/src/assets/images/penguin-blossom.png',
+    price: 162
+  }
+];
---
+++ src/data/reviews.data.ts
@@ -0,0 +1,23 @@
+export const reviewsMock = [
+  {
+    id: 1,
+    user: 'Emily Carter',
+    rating: 5,
+    comment: 'Wonderful product, impeccable finish! Highly recommend.',
+    date: '2025-07-20'
+  },
+  {
+    id: 2,
+    user: 'Michael Johnson',
+    rating: 4,
+    comment: 'Really liked it, arrived quickly and well packaged.',
+    date: '2025-07-18'
+  },
+  {
+    id: 3,
+    user: 'Olivia Smith',
+    rating: 5,
+    comment: 'Exceeded my expectations! Will buy again.',
+    date: '2025-07-15'
+  }
+];
